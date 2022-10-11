64fdb60ab4763c2c52594d725476a893d53fd492
Your Name <you@example.com>
2022-10-10 19:12:08 -0800

    finished and commiting e-commerce project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./component/Navigation";
 import Products from "./component/Products";
 import SingleProduct from "./component/SingleProduct";
